d4bb0fbb185d748eb33e60b2916105b51fe39983
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2020-10-07 17:52:53 -0500

    few more done

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -4,10 +4,38 @@ import random
 with open("input.txt") as f:
     words = f.read()
 
-# TODO: analyze which words can follow other words
-# Your code here
+# analyze which words can follow other words
 
 
-# TODO: construct 5 random sentences
-# Your code here
+def buildCache(string, cache={}):
+    wordList = string.split()
+    for i, word in enumerate(wordList):
+        if i + 1 != len(wordList):
+            if word not in cache:
+                cache[word] = []
+                cache[word].append(wordList[i + 1])
+            else:
+                if wordList[i + 1] not in cache[word]:
+                    cache[word].append(wordList[i + 1])
+    return cache
 
+
+# construct 5 random sentences
+def randomeSentence(d):
+    wordList = words.split()
+    fullSentence = []
+    endingList = ['!', '.', '?']
+    firstWord = random.choice(wordList)  # String
+    fullSentence.append(firstWord)
+    newWord = d[firstWord]  # Array
+
+    while fullSentence[-1][-1] not in endingList:
+        for word in newWord:
+            fullSentence.append(word)
+        nextWord = random.choice(wordList)
+        newWord = d[nextWord]
+
+    return " ".join(fullSentence)
+
+
+print(randomeSentence(buildCache(words)))
